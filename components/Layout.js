6ec50aa4059c7b9561b09336f676eee67836c132
danhdeng <hui_danny_deng@hotmail.com>
2021-11-21 12:27:24 -0500

    add MaterilaUI Theme

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,10 @@
 import {
   AppBar,
   Container,
+  createTheme,
+  CssBaseline,
   Link,
+  ThemeProvider,
   Toolbar,
   Typography,
 } from '@material-ui/core';
@@ -12,34 +15,63 @@ import useStyles from '../utils/styles';
 
 export default function Layout({ title, description, children }) {
   const classes = useStyles();
+  const theme = createTheme({
+    typography: {
+      h1: {
+        fontSize: '1.6rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      h2: {
+        fontSize: '1.4rem',
+        fontWeight: 400,
+        margin: '1rem 0',
+      },
+      body1: {
+        fontWeight: 'normal',
+      },
+    },
+    palette: {
+      type: 'light',
+      primary: {
+        main: '#f0c000',
+      },
+      secondary: {
+        main: '#208080',
+      },
+    },
+  });
   return (
     <div>
       <Head>
         <title>{title ? `${title} - Dan Store` : 'Dan Store'}</title>
         {description && <meta name="description" content={description}></meta>}
       </Head>
-      <AppBar position="static" className={classes.navbar}>
-        <Toolbar>
-          <NextLink href="/">
-            <Link>
-              <Typography className={classes.brand}>Dan Store</Typography>
-            </Link>
-          </NextLink>
-          <div className={classes.grow}></div>
-          <div>
-            <NextLink href="/cart" passHref>
-              <Link>Cart</Link>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <AppBar position="static" className={classes.navbar}>
+          <Toolbar>
+            <NextLink href="/">
+              <Link>
+                <Typography className={classes.brand}>Dan Store</Typography>
+              </Link>
             </NextLink>
-            <NextLink href="/login" passHref>
-              <Link>Login</Link>
-            </NextLink>
-          </div>
-        </Toolbar>
-      </AppBar>
-      <Container className={classes.main}>{children}</Container>
-      <footer className={classes.footer}>
-        <Typography>All Rights Reserved Dan Store</Typography>
-      </footer>
+            <div className={classes.grow}></div>
+            <div>
+              <NextLink href="/cart" passHref>
+                <Link>Cart</Link>
+              </NextLink>
+              <NextLink href="/login" passHref>
+                <Link>Login</Link>
+              </NextLink>
+            </div>
+          </Toolbar>
+        </AppBar>
+        <Container className={classes.main}>{children}</Container>
+        <footer className={classes.footer}>
+          <Typography>All Rights Reserved. Dan Store</Typography>
+        </footer>
+      </ThemeProvider>
     </div>
   );
 }
--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -45,7 +45,9 @@ export default function ProductScreen() {
         <Grid item md={3} xs={12}>
           <List>
             <ListItem>
-              <Typography component="h1">{product.name}</Typography>
+              <Typography component="h1" variant="h1">
+                {product.name}
+              </Typography>
             </ListItem>
             <ListItem>
               <Typography>Category: {product.category}</Typography>
@@ -82,7 +84,7 @@ export default function ProductScreen() {
                 </Grid>
                 <Grid item xs={6}>
                   <Typography>
-                    ${product.countInStock > 0 ? 'In stock' : 'Unavaiable'}
+                    {product.countInStock > 0 ? 'In stock' : 'Unavailable'}
                   </Typography>
                 </Grid>
               </ListItem>
