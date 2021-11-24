f32d88253ecf4f5d5bc09e18d1ab08b288abc245
danhdeng <hui_danny_deng@hotmail.com>
2021-11-23 19:39:34 -0500

    use react-form-hook to handle the validation in login page

--- pages/api/users/login.js
+++ pages/api/users/login.js
@@ -1,8 +1,8 @@
+import bcrypt from 'bcryptjs';
 import nc from 'next-connect';
 import User from '../../../models/User';
-import db from '../../../utils/db';
-import bcrypt from 'bcryptjs';
 import { signToken } from '../../../utils/auth';
+import db from '../../../utils/db';
 const handler = nc();
 
 handler.post(async (req, res) => {
@@ -11,6 +11,8 @@ handler.post(async (req, res) => {
   await db.disconnect();
   if (user && bcrypt.compareSync(req.body.password, user.password)) {
     const token = signToken(user);
+    console.log(token);
+    console.log(user);
     res.send({
       token,
       _id: user._id,
--- pages/login.js
+++ pages/login.js
@@ -9,22 +9,24 @@ import {
 import axios from 'axios';
 import NextLink from 'next/link';
 import { useRouter } from 'next/router';
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import Layout from '../components/Layout';
 import { Store } from '../utils/Store';
 import useStyles from '../utils/styles';
 
 export default function Login() {
   const { userInfo } = useContext(Store);
-
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm();
   const router = useRouter();
   const { redirect } = router.query;
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   const classes = useStyles();
   const { dispatch } = useContext(Store);
-  const loginHandler = async (e) => {
-    e.preventDefault();
+  const loginHandler = async ({email, password}) => {
     try {
       const { data } = await axios.post('/api/users/login', {
         email,
@@ -34,8 +36,9 @@ export default function Login() {
       //window.alert('login successfully');
       router.push(redirect || '/');
     } catch (error) {
+      console.log(error);
       window.alert(
-        error.response.data ? error.response.data.message : error.message
+        error.response ? error.response.data.message : error.message
       );
     }
   };
@@ -46,30 +49,68 @@ export default function Login() {
   }, [router, userInfo]);
   return (
     <Layout title="Login">
-      <form onSubmit={loginHandler} className={classes.form}>
+      <form onSubmit={handleSubmit(loginHandler)} className={classes.form}>
         <Typography component="h1" variant="h1">
           Login
         </Typography>
         <List>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="email"
-              label="Email"
-              inputProps={{ type: 'email' }}
-              onChange={(e) => setEmail(e.target.value)}
-            ></TextField>
+            <Controller
+              name="email"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                pattern: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="email"
+                  label="Email"
+                  inputProps={{ type: 'email' }}
+                  error={Boolean(errors.email)}
+                  helperText={
+                    errors.email
+                      ? errors.email.type === 'pattern'
+                        ? 'Email is not valid'
+                        : 'Email is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
-            <TextField
-              variant="outlined"
-              fullWidth
-              id="password"
-              label="Password"
-              inputProps={{ type: 'password' }}
-              onChange={(e) => setPassword(e.target.value)}
-            ></TextField>
+            <Controller
+              name="password"
+              control={control}
+              defaultValue=""
+              rules={{
+                required: true,
+                minLength: 6,
+              }}
+              render={({ field }) => (
+                <TextField
+                  variant="outlined"
+                  fullWidth
+                  id="password"
+                  label="Password"
+                  inputProps={{ type: 'password' }}
+                  error={Boolean(errors.password)}
+                  helperText={
+                    errors.password
+                      ? errors.password.type === 'minLength'
+                        ? 'Password length must be more than 5'
+                        : 'Password is required'
+                      : ''
+                  }
+                  {...field}
+                ></TextField>
+              )}
+            ></Controller>
           </ListItem>
           <ListItem>
             <Button variant="contained" type="submit" fullWidth color="primary">
@@ -77,8 +118,8 @@ export default function Login() {
             </Button>
           </ListItem>
           <ListItem>
-            Don't have an account? &nbsp;
-            <NextLink href="/register">
+            Don&apos;t have an account? &nbsp;
+            <NextLink href={`/register?redirect=${redirect || '/'}`} passHref>
               <Link>Register</Link>
             </NextLink>
           </ListItem>
